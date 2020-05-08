76e7a3623c9e51636e1dd1d56970a4edf844bdd9
Warren LaFrance <warrenlafrance@outlook.com>
2020-05-08 09:06:44 -0500

    Initial Init

---
+++ src/components/hello-world.vue
@@ -0,0 +1,34 @@
+<template>
+ <div>
+   <h1>Hello</h1>
+   <v-btn>Hello</v-btn>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { VBtn } from 'vuetify/lib'
+import vuetify from '../plugins/vuetify';
+
+export default Vue.extend({
+  name: "HelloWorld",
+  components:{
+    VBtn
+  },
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+       select: ['Vuetify', 'Programming'],
+       items:[
+          'Programming', 'Design', 'Vue','Vuetify',
+       ],
+       list:[]
+    }
+  }
+});
+</script>
+<style scoped>
+?????
+</style>
